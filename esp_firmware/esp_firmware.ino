1e7e2a267311901eaf90707153918fd9b6369fb4
Marcin Malessa <marcin@malessa.pl>
2018-03-16 12:54:55 +0100

    Example from DHT library

--- esp_firmware/esp_firmware.ino
+++ esp_firmware/esp_firmware.ino
@@ -2,16 +2,53 @@
  *   ESP DHT: firmware
  *   2018 mmalessa http://www.malessa.pl/
  */
- 
-#include "esp_dht.h"
 
+
+
+// Example from DHT library.
+
+#include "DHT.h"
+
+#define DHTPIN 2     // what digital pin we're connected to
+
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial.println("DHTxx test!");
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Wait a few seconds between measurements.
+  delay(2000);
 
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C \t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C \n");
 }
